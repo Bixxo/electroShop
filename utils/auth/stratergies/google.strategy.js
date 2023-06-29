335570bcba47f755e4eae84b1676b1f53e1b9b54
Adrian <adrian_ebr@hotmail.com>
2023-06-28 19:42:38 -0600

    se agrega url auth google

--- utils/auth/stratergies/google.strategy.js
+++ utils/auth/stratergies/google.strategy.js
@@ -8,7 +8,7 @@ const userService = new UserService();
 const GoogleStrategy = new Strategy({
         clientID: config.googleId,
         clientSecret: config.googleSecret,
-        callbackURL: 'http://localhost:8080/api/v1/auth/login/google'
+        callbackURL: 'https://electroshop-api.onrender.com/api/v1/auth/login/google'
     },
     async (accessToken, refreshToken, profile, done) => {
         const user = await userService.authByGoogleId(profile.id);
