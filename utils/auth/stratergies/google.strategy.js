476620b870d98fc3ba8e9dcb38b420f06568d2e1
Adrian <adrian_ebr@hotmail.com>
2023-06-28 19:57:40 -0600

    pruebas2

--- utils/auth/stratergies/google.strategy.js
+++ utils/auth/stratergies/google.strategy.js
@@ -8,25 +8,11 @@ const userService = new UserService();
 const GoogleStrategy = new Strategy({
         clientID: config.googleId,
         clientSecret: config.googleSecret,
+        // callbackURL: 'http://localhost:8080/api/v1/auth/login/google',
         callbackURL: 'https://electroshop-api.onrender.com/api/v1/auth/login/google'
     },
     async (accessToken, refreshToken, profile, done) => {
-        const user = await userService.authByGoogleId(profile.id);
-        if(!user) {
-            await userService.authCreateGoogleUser({
-                name: profile.displayName,
-                email: profile.emails[0].value,
-                googleId: profile.id,
-                admin: false,
-                active: false,
-                password: config.googlePass
-            });
-        }
-        try {
-            done(null, {status: 1, profile});
-        } catch (error) {
-            done(error, null);
-        }
+        done(null, profile);
     }
 );
 
