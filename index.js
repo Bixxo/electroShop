2553b006b8499a59ab44cbab160bef9ec453be75
Luciano Fredes <lucianogonzalofredes@gmail.com>
2023-07-05 11:56:25 -0300

    cambios CORS

--- index.js
+++ index.js
@@ -34,10 +34,10 @@ app.use('/images', express.static('./uploads'));
 app.use(morgan('dev'));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-    // res.header('Access-Control-Allow-Credentials', 'true');
-    // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    // res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
-    // next();
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
+    next();
     });
 
 
